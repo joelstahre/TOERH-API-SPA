3f5433da1f81de89c799e2344eb17aac745a77e3
Joel Stahre <joelstahre@gmail.com>
2014-01-31 01:33:36 +0100

    Add new project

---
+++ TOERH/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TOERH::Application.config.secret_key_base = '142cf49d1e0e42dc68740122bcf8e7794b4e97311167abb88ddb12560daa44397d8d452b7f5a5fc3e4f93f909c921559514dc66394e26a1a86c185dec45c0f73'
